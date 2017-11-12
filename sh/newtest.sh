76d2a7341fe13cc89f5ce106a4fae21f2267975e
agz1117 <agz1117@hotmail.com>
2017-11-12 09:53:48 -0800

    update

--- sh/newtest.sh
+++ sh/newtest.sh
@@ -3,3 +3,15 @@
 
 # Andrew Zhang
 # Performance Test
+i#!/bin/bash
+
+#: Script name: jaguar_init.sh
+#: Authors: Andrew Zhang
+#: Reviewers:
+#: Date: 10/02/2017
+#: Purpose:     Cleanup the jaguar db and setup a cleaned test environment.
+#: Test cases:
+#       Use case 1:
+#       -------------------------------------------------------------------
+#
+
