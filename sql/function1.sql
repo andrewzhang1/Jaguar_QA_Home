0357ea091ef27135a04473bf38663a607d304bb2
agz1117 <agz1117@hotmail.com>
2017-11-20 23:34:33 -0800

    added function1

--- sql/function1.sql
+++ sql/function1.sql
@@ -1,30 +1,42 @@
 # SQL Name: function1.sql 
 
+drop table if exists function1;
+
 spool  $QA_HOME/work/function1.out; 
 
-create table function1 ( key: ID_Num char(32), value: Random1 float(2, 20), Random2 float(2, 20), Random3 float(2, 20), Random4 float(2, 20), Random5 float(2, 20);
+create table function1 ( key: ID_Num char(32), Random1 double(30,28),Random2 double(30,28),Random3 double(30,28),Random4 double(30,28),Random5 double(30,28));
 
-desc function1;
+desc function1;  
 
-load $QA_HOME/data/carPlate.txt into function1;
-select count(*) from function1;
-
-spool off;
-quit;
-
-
-load $QA_HOME/data/10kLine.txt into data_load;
+load $QA_HOME/data/carPlate.txt into function1 ;
 
 sleep 5;
 
-select count(*) from  data_load;
-select * from data_load  where v3 = "Apple31234567890";
-select * from data_load  where v3 = 'Apple31234567890';
 
-show create table data_load;
-drop table data_load;
+select count(*) from function1;
 
-#spool off;
+# test functions:
+
+select sum(random1)  from test.function1;
+select sum(random1) as sum_random1  from test.function1 limit 3;
+select avg(random1)  from test.function1;
+select min(random1)  from test.function1;
+select max(random1)  from test.function1;
+select count(random1)  from test.function1;
+select stddev(random1)  from test.function1;
+select first(random1)  from test.function1;
+select last(random1)  from test.function1;
+select abs(random1)  from test.function1 limit 10;
+select acos(random1)  from test.function1 limit 10;
+select upper(id_num)  from test.function1 limit 2;
+
+select substring(random1,1 ,3 )  from test.function1 limit 2;
+select date(random1 )  from test.function1 limit 2;
+
+select cos(random1), sin(random2)  from test.function1 limit 3;
+
+
+spool off;
 quit;
 
 
